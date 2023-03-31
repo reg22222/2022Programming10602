ce039020167bc89e2c89ea6673a881881ca0ffa9
Reg22222 <103514126+reg22222@users.noreply.github.com>
2023-03-31 16:03:54 +0900

    Add files via upload

---
+++ JavaGod0331.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class a{
+
+    public int a;
+
+    
+
+      public a(){
+
+        a = 20;
+    }
+
+
+
+    //멤버메소드
+    public void disp(){
+        System.out.println("a = "+a);
+
+    }
+
+    //생성자
+
+    class B{
+
+    }
+  
+
+}
+
+public class JavaGod0331{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    
+        a ap = new a();
+
+
+        ap.disp();
+
+  
+
+
+    }
+}
---
+++ java.java
@@ -0,0 +1,23 @@
+class A01{
+
+    int a;
+    int b;
+
+    public A01(int a, int b){
+
+        this.a = 10;
+        this.b = 10;
+    }
+
+
+    
+}
+
+public class java {
+    public static void main(String[] args) {
+        
+
+        int a = 10;
+    }
+
+}
